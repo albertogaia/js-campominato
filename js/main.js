b8295c4caa9075ba480328fa16e086fbf517c8cf
albertogaia <alberto.gaia95@gmail.com>
2021-07-26 14:52:46 +0200

    alert con punteggio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
 var items = 5;
 var bombs= [];
-var numMin = 1;
-var numMax = 100;
+const numMin = 1;
+const numMax = 10;
 
 function randomGen (min, max){
     while(bombs.length < items) {
@@ -17,8 +17,34 @@ function randomGen (min, max){
 console.log(randomGen(numMin, numMax));
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-
 // L’utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+var itemsUser = numMax - items
+var userNumbers = [];
+var punteggio = 0;
+
+while(userNumbers.length < itemsUser){
+    var numUser = parseInt(prompt('Inserisci un numero tra ' + numMin + ' e ' + numMax));
+    console.log(numUser);
+    if ((userNumbers.indexOf(numUser) == -1) && (isNaN(numUser) == false) && (numUser >= numMin && numUser <= numMax)){
+        userNumbers.push(numUser);
+        punteggio += 1;
+    }
+    // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+    if(bombs.includes(numUser)){
+        alert('Hai perso, che peccato!');
+        punteggio = punteggio - 1;
+        break;
+    }
+    // La partita termina quando il giocatore raggiunge il numero massimo possibile di numeri consentiti.
+    if(userNumbers.length == itemsUser){
+        alert('Grande! Hai vinto!!!')
+        break
+    }
+}
+
+console.log(userNumbers);
+
+
+
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+alert('Il tuo Punteggio: ' + punteggio);
